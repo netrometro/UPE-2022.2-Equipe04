cc00aced5910fa9931aa141c7980a90e1e968716
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-03-05 14:03:05 -0300

    setup back-end

---
+++ develop/gerenciador_de_loja/src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
+
+    fastify.get('/', async () => {
+        const count = await prisma.user.count()
+        return {hello: 'world'}
+    })
+
+    await fastify.listen({port: 3333})
+}
+
+bootstrap()
