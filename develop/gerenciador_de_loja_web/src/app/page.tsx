16d5af22b0799f0f07789e1d3831fb615171355e
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-03-09 09:48:30 -0300

    setup front

--- develop/gerenciador_de_loja_web/src/app/page.tsx
+++ develop/gerenciador_de_loja_web/src/app/page.tsx
@@ -1,5 +1,15 @@
+import { Teste } from "../../components/teste";
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+
+
+    <><h1>Hello world</h1>
+    
+    <div>
+      <Teste />
+    </div></>
+
   )
 }
+
