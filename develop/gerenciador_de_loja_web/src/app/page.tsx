8e6ad01bc8c11e5168d8982520919b1ddca63ba5
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-03-09 11:13:15 -0300

    header e botão de entrar

--- develop/gerenciador_de_loja_web/src/app/page.tsx
+++ develop/gerenciador_de_loja_web/src/app/page.tsx
@@ -1,14 +1,23 @@
+'use client';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { HeaderPage } from "../../components/header";
 import { Teste } from "../../components/teste";
+import { padding } from '@mui/system';
 
 export default function Home() {
   return (
 
+  <>   
+    <HeaderPage /> 
 
-    <><h1>Hello world</h1>
-    
-    <div>
-      <Teste />
-    </div></>
+    <div className='home-div'>
+      <Button variant="contained" size="large">
+          Entrar
+      </Button>
+    </div>
+  </>
 
   )
 }
