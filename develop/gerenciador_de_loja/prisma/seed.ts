41ac82bcf2e7c9072005d0b6f57bd69309edd5f7
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-03-12 17:06:14 -0300

    migration and seed

---
+++ develop/gerenciador_de_loja/prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({ 
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            password: '123456',
+            role: 'ADMIN',  
+            
+        }
+    })
+
+    const supplier = await prisma.supplier.create({
+        data: {
+            name: 'Fornecedor 1',            
+            phone: '11 99999-9999',
+            info: 'Descrição do fornecedor 1',
+            products: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+
+    })
+
+    const product = await prisma.product.create({
+        data: {
+            name: 'Produto 1',
+            price: 10.50,
+            info: 'Descrição do produto 1',
+            photo: 'https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            supplier: {
+                connect: {
+                    id: 1
+            }
+        }
+    }
+    })
+    
+
+main()}
--- develop/gerenciador_de_loja/src/server.ts
+++ develop/gerenciador_de_loja/src/server.ts
@@ -1,6 +1,8 @@
-import Fastify from 'fastify'
+import Fastify, { fastify } from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import bcrypt from 'fastify-bcrypt';
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -22,5 +24,24 @@ async function bootstrap() {
 
     await fastify.listen({port: 3333})
 }
+
+const app = fastify();
+
+app.post('/login', async (request, reply) => {
+  const { email, password } = request.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    reply.status(401).send({ error: 'Email ou senha inválidos' });
+    return;
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+
+  reply.send({ user, token });
+});
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
 
 bootstrap()
