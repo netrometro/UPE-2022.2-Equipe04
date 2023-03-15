e0ff6adf17b5cd4277960a1ee6978229b7406c74
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-03-15 00:24:54 -0300

    inicio box login

--- develop/gerenciador_de_loja_web/src/app/page.tsx
+++ develop/gerenciador_de_loja_web/src/app/page.tsx
@@ -1,5 +1,8 @@
 'use client';
+import type { NextPage } from 'next'
+import Head from 'next/head'
 import * as React from 'react';
+import LoginForm from '../../components/login-form'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { HeaderPage } from "../../components/header";
@@ -12,11 +15,16 @@ export default function Home() {
   <>   
     <HeaderPage /> 
 
-    <div className='home-div'>
-      <Button variant="contained" size="large">
-          Entrar
-      </Button>
+    <div>
+      <Head>
+        <title>Entrar</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <LoginForm/>
+      </main>
     </div>
+
   </>
 
   )
