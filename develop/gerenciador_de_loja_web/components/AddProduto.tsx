f5e233284c331c715e8113382315710e396afd01
Camila Claudino <camila.claudino@upe.br>
2023-03-18 11:33:39 -0300

    Atualizando o cadastro de produtos

--- develop/gerenciador_de_loja_web/components/AddProduto.tsx
+++ develop/gerenciador_de_loja_web/components/AddProduto.tsx
@@ -5,38 +5,38 @@ import { FormEvent, useState } from "react";
 
 export function Produtos(){
   
-  const [tipoProduto, setTipoProduto] = useState('');
-  const [tamanhoProduto, setTamanhoProduto] = useState('');
-  const [corProduto, setCorProduto] = useState('');
-  const [materialProduto, setMaterialProduto] = useState('');
-  const [marcaProduto, setMarcaProduto] = useState('');
-  const [quantidadeProduto, setQuantidadeProduto] = useState(0);
-  const [precoProduto, setPrecoProduto] = useState(0.0);
+  const [productName, setproductName] = useState('');
+  const [productSize, setproductSize] = useState('');
+  const [productPhoto, setproductPhoto] = useState('');
+  const [productSupplier, setproductSupplier] = useState('');
+  const [productInfo, setproductInfo] = useState('');
+  const [productStock, setproductStock] = useState(0);
+  const [productPrice, setproductPrice] = useState(0.0);
   
-  function createProduto(event: FormEvent){
+  function createProduct(event: FormEvent){
     event.preventDefault();
     try{
 
-      const response = api.post('/produtos/create', {
-        tipo: tipoProduto,
-        tamanho: tamanhoProduto,
-        cor: corProduto,
-        material: materialProduto,
-        marca: marcaProduto,
-        quantidade: quantidadeProduto,
-        preco: precoProduto,
+      const response = api.post('/products/create', {
+        name: productName,
+        size: productSize,
+        photo: productPhoto,
+        supplier: productSupplier,
+        info: productInfo,
+        stock: productStock,
+        price: productPrice,
 
       });
       alert('Produto cadastrado com sucesso');
       console.log( response);
     
-      setTipoProduto('');
-      setTamanhoProduto('');
-      setCorProduto('');
-      setMaterialProduto('');
-      setMarcaProduto('');
-      setQuantidadeProduto(0);
-      setPrecoProduto(0.0);
+      setproductName('');
+      setproductSize('');
+      setproductPhoto('');
+      setproductSupplier('');
+      setproductInfo('');
+      setproductStock(0);
+      setproductPrice(0.0);
 
     }catch(err){
       console.log(err);
@@ -46,21 +46,21 @@ export function Produtos(){
     
   return (
     <div> 
-      <form onSubmit={createProduto}>
+      <form onSubmit={createProduct}>
         
-        <input type="text" placeholder="Tipo do produto" onChange={event => setTipoProduto(event.target.value)} value={tipoProduto}/>
+        <input type="text" placeholder="name do produto" onChange={event => setproductName(event.target.value)} value={productName}/>
         <br />
-        <input type="text" placeholder="Tamanho do produto" onChange={event => setTamanhoProduto(event.target.value)} value={tamanhoProduto}/>
+        <input type="text" placeholder="size do produto" onChange={event => setproductSize(event.target.value)} value={productSize}/>
         <br />
-        <input type="text" placeholder="Cor do produto" onChange={event => setCorProduto(event.target.value)} value={corProduto}/>
+        <input type="text" placeholder="photo do produto" onChange={event => setproductPhoto(event.target.value)} value={productPhoto}/>
         <br />
-        <input type="text" placeholder="Material do produto" onChange={event => setMaterialProduto(event.target.value)} value={materialProduto}/>
+        <input type="text" placeholder="supplier do produto" onChange={event => setproductSupplier(event.target.value)} value={productSupplier}/>
         <br />
-        <input type="text" placeholder="Marca do produto" onChange={event => setMarcaProduto(event.target.value)} value={marcaProduto}/>
+        <input type="text" placeholder="info do produto" onChange={event => setproductInfo(event.target.value)} value={productInfo}/>
         <br />
-        <input type="number" placeholder="Quantidade do produto" onChange={event => setQuantidadeProduto(parseInt(event.target.value))} value={quantidadeProduto}/>
+        <input type="number" placeholder="stock do produto" onChange={event => setproductStock(parseInt(event.target.value))} value={productStock}/>
         <br />
-        <input type="number" placeholder="Preço do produto" onChange={event => setPrecoProduto(parseFloat(event.target.value))} value={precoProduto}/> 
+        <input type="number" placeholder="Preço do produto" onChange={event => setproductPrice(parseFloat(event.target.value))} value={productPrice}/> 
         <br />
         <br />
 
@@ -75,8 +75,8 @@ export function Produtos(){
 export async function getServerSideProps() {
 
   const [ProdutosCreateResponse, ListProdutosResponse] = await Promise.all([
-    api.post('/produtos/create'),
-    api.get('/produtos'),
+    api.post('/products/create'),
+    api.get('/products'),
   ]);
 
   return {
--- develop/gerenciador_de_loja_web/components/login-form.tsx
+++ develop/gerenciador_de_loja_web/components/login-form.tsx
@@ -43,4 +43,5 @@ export default function LoginForm() {
         </Formik>
       </div>
     );
+    
 }
