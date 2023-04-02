c6dd519bf4426ef94269c347cb7c56228a8e5b68
Camila Claudino <camila.claudino@upe.br>
2023-04-02 18:11:47 -0300

    Revert "Merge branch 'Anna-Semana1'"
    This reverts commit 0c4f8a1948134c7f89fd702f95e15e616111b95b, reversing
    changes made to c44844e99eb877913477103a2e2ed3830c2a00d2.

--- develop/gerenciador_de_loja/src/server.ts
+++ develop/gerenciador_de_loja/src/server.ts
@@ -1,17 +1,26 @@
-import Fastify, { fastify } from 'fastify'
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
-import { usersRoutes } from './routes/user';
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    await fastify.register(usersRoutes)
-
     await fastify.register(cors, {
         origin: true,
     })
 
+    fastify.get('/', async () => {
+        const count = await prisma.user.count()
+        return {count}
+    })
+
     await fastify.listen({port: 3333})
 }
+
+bootstrap()
--- develop/gerenciador_de_loja_web/src/app/page.tsx
+++ develop/gerenciador_de_loja_web/src/app/page.tsx
@@ -1,31 +1,14 @@
-'use client';
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import * as React from 'react';
-import LoginForm from '../../components/login-form'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { HeaderPage } from "../../components/header";
 import { Teste } from "../../components/teste";
-import { padding } from '@mui/system';
 
 export default function Home() {
   return (
 
-  <>   
-    <HeaderPage /> 
 
+    <><h1>Hello world</h1>
+    
     <div>
-      <Head>
-        <title>Entrar</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <LoginForm/>
-      </main>
-    </div>
-
-  </>
+      <Teste />
+    </div></>
 
   )
 }
